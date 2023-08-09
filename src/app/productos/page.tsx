d14ff6ee0820abcdbe7b03998b0fe95d1eb7fc43
causapablo@gmail.com <causapablo@gmail.com>
2023-08-09 20:45:34 -0300

    Componente de noticias, blog y productos

--- src/app/productos/page.tsx
+++ src/app/productos/page.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import categorias from '../../../public/categorias.json';
-import mate from '../../../public/images/mate.jpg';
-import te from '../../../public/images/especias.jpg';
-import spices from '../../../public/images/spices.jpg';
-import oil from '../../../public/images/oil.jpg';
-import granos from '../../../public/images/granos.png';
+
 import Burbuja from '../../../components/burbuja/Burbuja';
 
 function Productos() {
@@ -14,10 +10,10 @@ function Productos() {
         <div className='bg-gradient-to-b from-peach to-white pt-32'>
             <section className='container mx-auto max-w-[1333px]'>
                 <h1 className='bg-red font-Poppins text-white inline-block px-[10px] py-1 text-[20px] font-[700]'>Bulk Products</h1>
-                <p></p>
+                
                 <div className="grid grid-cols-2 gap-4 pt-2">
 
-                    <div>
+                    <div className=''>
                         <p className='font-Fitgree text-[17px] text-justify font-[500]'>
                             Here you’ll find a wide selection of specialty raw ingredients ranging from coconut products and plant-based proteins to sweeteners and additives. You can also browse by organic, halal, kosher, and vegan subcategories. Looking for something not listed?
                         </p>

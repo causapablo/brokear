7a14f01777d874d976109fc4f30d0649124d31a6
solanamr <romerosolana@gmail.com>
2023-09-06 15:27:37 -0300

    commiting so I can pull

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Footer from '../../components/footer/Footer'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        
       <NavBar/>
         {children}
         <Footer/>
